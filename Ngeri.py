929dde37e39d7feadc0af5da511c4fb3c380b3fd
ReyZ3r <92297361+ReyZ3r@users.noreply.github.com>
2021-11-02 18:09:55 +0700

    Ngeri.py

---
+++ Ngeri.py
@@ -0,0 +1,18 @@
+import base64
+
+import sys
+
+from base64 import *
+
+with open(sys.argv[1],'r') as my_file:
+
+        data = my_file.read()
+
+for i in range (0,5):
+
+        data = base64.b16decode(Ngeri.py)
+
+print(data) 
+
+my_file.close()
+
